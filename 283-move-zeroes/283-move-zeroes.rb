7161635363647b13ffb0b5e94f2bae7c3da39ec4
Geisson <60269749+Geisson19@users.noreply.github.com>
2022-09-30 00:44:52 -0500

    Time: 269 ms (19.64%), Space: 212.9 MB (19.03%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.rb
+++ 283-move-zeroes/283-move-zeroes.rb
@@ -1,24 +1,12 @@
 
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
-
-def move_zeroes(nums)
-    n = nums.length
-    next_nonzero_place = 0
-    curr = 0
-    
-    while curr < n
-        if nums[curr] != 0
-            nums[next_nonzero_place] = nums[curr]
-            next_nonzero_place += 1
-        end
-        curr+=1
+def move_zeroes(nums = [])
+  j = 0
+  (0...nums.length).each do |i|
+    unless nums[i] == 0
+      nums[i], nums[j] = nums[j], nums[i]
+      j += 1
     end
-    
-    while next_nonzero_place < n
-        nums[next_nonzero_place] = 0
-        next_nonzero_place +=1
-    end
-        
+  end
 end
-
